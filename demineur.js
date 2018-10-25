a0f45171bfa9ebfba8e269e5a1516cf825c35c26
MercureTony <mercuretony@gmail.com>
2018-10-25 18:07:13 -0400

    Update demineur.js

--- demineur.js
+++ demineur.js
@@ -7,21 +7,21 @@ load("image.js");
 
 var afficherImage = function(x,y,colormap,image)
 {
-    var selection = images[image]; // On choisit l'image qu'on veut afficher
+    var selection = images[image];
     var hauteur = selection.length ; // Hauteur du tableau images
     var largeur = selection[0].length ; // Largeur du tableau images ** j'ai pris 0 comme index
     
-    setScreenMode(hauteur, largeur); // On defini la taille de l'image
+    setScreenMode(hauteur, largeur);
     
-    for(var i = 0 ; i<hauteur; i++) // La hauteur de notre tableau
+    for(var i = 0 ; i<hauteur; i++)
     {
-        for(var j = 0 ; j<largeur; j++) // La largeur de notre tableau 
+        for(var j = 0 ; j<largeur; j++)
         {
            	var couleur = selection[j][i]; // On colorie la surface ixj
            
-            if (i == x && j == y)//si on atteinnt le coordonné (x,y)
+            if (i == x && j == y)
             {
-            setPixel(x , y , colormap[couleur]); //on l'affiche
+            setPixel(x , y , colormap[couleur]);
             }
         }
     }
